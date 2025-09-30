e960bbc9aecdbe20977c61da4ffa05a112a317b7
L.Widdy <36073831+CodeurCopieur@users.noreply.github.com>
2025-09-30 10:39:26 +0200

    exercice 2

--- revision.js
+++ revision.js
@@ -3,3 +3,17 @@ var names;
 var speed = 25;
 var isLoading;
 var age;
+// exercice 2
+var colors = [];
+colors.push("red");
+console.log(colors);
+var infos = [];
+infos.push("string");
+infos.push(true);
+infos.push(40);
+console.log(infos);
+var infos2 = [];
+infos2.push("string");
+infos2.push(true);
+infos2.push(40);
+console.log(infos2);
--- revision.ts
+++ revision.ts
@@ -6,3 +6,19 @@ let isLoading: boolean;
 let age: string|number;
 
 
+// exercice 2
+let colors: string[] = [];
+colors.push("red");
+console.log(colors);
+
+let infos: Array<string|number|boolean> = [];
+infos.push("string");
+infos.push(true);
+infos.push(40);
+console.log(infos);
+
+let infos2: (string|number|boolean)[] = [];
+infos2.push("string");
+infos2.push(true);
+infos2.push(40);
+console.log(infos2);
